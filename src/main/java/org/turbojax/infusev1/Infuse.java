a97998246b50bb118d95d6d9457c79ea1e89859e
TurboJax <turbojax07@gmail.com>
2026-07-20 22:29:42 -0400

    Registering the recipes for the custom items

--- src/main/java/org/turbojax/infusev1/Infuse.java
+++ src/main/java/org/turbojax/infusev1/Infuse.java
@@ -1,8 +1,14 @@
 package org.turbojax.infusev1;
 
+import java.util.stream.Stream;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.turbojax.infusev1.items.Enhancer;
+import org.turbojax.infusev1.items.InfuseEffect;
+import org.turbojax.infusev1.items.Reviver;
 
 public class Infuse extends JavaPlugin {
     public static final Logger LOGGER = LoggerFactory.getLogger("Infuse");
@@ -19,5 +25,14 @@ public class Infuse extends JavaPlugin {
 
     public void onDisable() {
         LOGGER.info("Infuse Plugin has been disabled!");
+    }
+
+    public void registerRecipes() {
+        Stream.of(new InfuseEffect().createRecipe(), new Enhancer().createRecipe(), new Reviver().createRecipe())
+            .forEach(r -> {
+                Bukkit.removeRecipe(r.getKey());
+                Bukkit.addRecipe(r);
+                Bukkit.getOnlinePlayers().forEach(p -> p.discoverRecipe(r.getKey()));
+            });
     }
 }
