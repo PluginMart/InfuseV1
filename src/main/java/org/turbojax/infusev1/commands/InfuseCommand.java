c40cf3e3633378782dc98cd852e4c82feba4f107
TurboJax <turbojax07@gmail.com>
2026-07-23 00:34:37 -0400

    Adding multi-target support for the infuse commands and adjusting some parameters.

--- src/main/java/org/turbojax/infusev1/commands/InfuseCommand.java
+++ src/main/java/org/turbojax/infusev1/commands/InfuseCommand.java
@@ -1,7 +1,6 @@
 package org.turbojax.infusev1.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
@@ -13,10 +12,10 @@ import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSele
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.turbojax.infusev1.DataManager;
 import org.turbojax.infusev1.MainConfig;
 import org.turbojax.infusev1.items.CustomItem;
@@ -24,6 +23,7 @@ import org.turbojax.infusev1.util.BannedPlayerArgumentType;
 import org.turbojax.infusev1.util.InfuseItemArgumentType;
 
 import java.util.Date;
+import java.util.List;
 
 public class InfuseCommand {
     private static final MiniMessage mm = MiniMessage.miniMessage();
@@ -35,7 +35,7 @@ public class InfuseCommand {
             <aqua> |- revive <gold>\\<player><white>: Revives a dead player
             <aqua> |- setscore <gold>\\<player> \\<score><white>: Sets a player's score.  Also rerolls their effects.
             <aqua> |- getscore <gold>\\<player><white>: Gets a player's score
-            <aqua> \\- give <gold>\\<player> \\<item><white>: Gives a player an infuse item.
+            <aqua> \\- give <gold>\\<player> \\<item> [count]<white>: Gives a plasyer an infuse item.
             """);
 
     public static LiteralCommandNode<CommandSourceStack> build() {
@@ -52,21 +52,24 @@ public class InfuseCommand {
                 )
             )
             .then(Commands.literal("getscore")
-                .then(Commands.argument("player", StringArgumentType.word())
-                    .executes(c -> getScore(c, c.getArgument("player", String.class)))
+                .then(Commands.argument("player", ArgumentTypes.players())
+                    .executes(c -> getScore(c, c.getArgument("player", PlayerSelectorArgumentResolver.class)))
                 )
             )
             .then(Commands.literal("setscore")
-                .then(Commands.argument("player", ArgumentTypes.player())
+                .then(Commands.argument("player", ArgumentTypes.players())
                     .then(Commands.argument("score", IntegerArgumentType.integer(MainConfig.minScore(), MainConfig.maxScore()))
                         .executes(c -> setScore(c, c.getArgument("player", PlayerSelectorArgumentResolver.class), c.getArgument("score", Integer.class)))
                     )
                 )
             )
             .then(Commands.literal("give")
-                .then(Commands.argument("player", ArgumentTypes.player())
+                .then(Commands.argument("player", ArgumentTypes.players())
                     .then(Commands.argument("item", new InfuseItemArgumentType())
-                        .executes(c -> give(c, c.getArgument("player", PlayerSelectorArgumentResolver.class), c.getArgument("item", CustomItem.class)))
+                        .executes(c -> give(c, c.getArgument("player", PlayerSelectorArgumentResolver.class), c.getArgument("item", CustomItem.class), 1))
+                        .then(Commands.argument("count", IntegerArgumentType.integer(1))
+                            .executes(c -> give(c, c.getArgument("player", PlayerSelectorArgumentResolver.class), c.getArgument("item", CustomItem.class), c.getArgument("count", Integer.class)))
+                        )
                     )
                 )
             )
@@ -102,48 +105,66 @@ public class InfuseCommand {
         return 1;
     }
 
-    public static int getScore(CommandContext<CommandSourceStack> ctx, String target) {
+    public static int getScore(CommandContext<CommandSourceStack> ctx, PlayerSelectorArgumentResolver resolver) {
         CommandSender sender = ctx.getSource().getSender();
-        OfflinePlayer player = Bukkit.getOfflinePlayer(target);
 
-        sender.sendMessage(player.getName() + "'s score is " + DataManager.getScore(player));
+        List<Player> targets;
+
+        try {
+            targets = resolver.resolve(ctx.getSource());
+        } catch (CommandSyntaxException e) {
+            sender.sendMessage(msgSerializer.deserialize(e.getRawMessage()));
+            return 1;
+        }
+        
+        targets.forEach(p -> sender.sendMessage(p.getName() + "'s score is " + DataManager.getScore(p)));
+
         return 1;
     }
 
     public static int setScore(CommandContext<CommandSourceStack> ctx, PlayerSelectorArgumentResolver resolver, int score) {
         CommandSender sender = ctx.getSource().getSender();
-        Player player;
+        List<Player> targets;
 
         try {
-            player = resolver.resolve(ctx.getSource()).getFirst();
+            targets = resolver.resolve(ctx.getSource());
         } catch (CommandSyntaxException e) {
             sender.sendMessage(msgSerializer.deserialize(e.getRawMessage()));
             return 1;
         }
 
-        DataManager.setScore(player, score);
-        DataManager.resetEffects(player);
+        targets.forEach(player -> {
+            DataManager.setScore(player, score);
+            DataManager.resetEffects(player);
 
-        // Banning the player if necessary
-        int banScore = MainConfig.banScore();
-        if (banScore < 0 && score == banScore) {
-            player.ban("Ran out of lives!", (Date) null, null);
-            DataManager.ban(player);
-        }
+            // Banning the player if necessary
+            int banScore = MainConfig.banScore();
+            if (banScore < 0 && score == banScore) {
+                player.ban("Ran out of lives!", (Date) null, null);
+                DataManager.ban(player);
+            }
+        });
 
-        sender.sendMessage(Component.text("Set " + player.getName() + "'s score to " + score, NamedTextColor.GREEN));
+        sender.sendMessage(Component.text("Set " + (targets.size() == 1 ? targets.getFirst().getName() + "'s score" : targets.size() + " player's scores") + " to " + score, NamedTextColor.GREEN));
 
         return 1;
     }
 
-    public static int give(CommandContext<CommandSourceStack> ctx, PlayerSelectorArgumentResolver resolver, CustomItem item) {
+    public static int give(CommandContext<CommandSourceStack> ctx, PlayerSelectorArgumentResolver resolver, CustomItem item, int count) {
         CommandSender sender = ctx.getSource().getSender();
 
         try {
-            Player p = resolver.resolve(ctx.getSource()).getFirst();
-            p.give(item.createItem());
+            List<Player> players = resolver.resolve(ctx.getSource());
+            
+            players.stream()
+                .forEach(p -> {
+                    ItemStack stack = item.createItem();
+                    stack.setAmount(count);
 
-            sender.sendMessage("Gave " + p.getName() + " a " + item.getKey().getKey());
+                    p.give(stack);
+                });
+
+            sender.sendMessage("Gave " + (players.size() == 1 ? players.getFirst().getName() : players.size() + " players") + " " + (count == 1 ? "a" : count) + " " + item.getKey().getKey() + ( count == 1 ? "" : "s"));
         } catch (CommandSyntaxException e) {
             sender.sendMessage(msgSerializer.deserialize(e.getRawMessage()));
         }
