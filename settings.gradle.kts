056724e64c6b8822fb6565d651638cdf15450045
TurboJax <turbojax07@gmail.com>
2026-08-19 00:49:07 -0400

    Removing extra stuff from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,10 @@ pluginManagement {
 
     repositories {
         mavenCentral()
-        gradlePluginPortal()
-        maven("https://maven.fabricmc.net/")
-    }
-
-    plugins {
-        id("net.fabricmc.fabric-loom") version providers.gradleProperty("loom_version")
-    }
-}
-
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
         maven("https://maven.fabricmc.net/")
     }
 }
 
-// Should match your modid
 rootProject.name = "infusev1"
 
 include("api", "impl")
