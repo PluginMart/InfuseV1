5bead56ffa48c9576ea5e5fb9a9ca30998e7efa8
TurboJax <turbojax07@gmail.com>
2026-09-06 12:48:00 -0400

    Fixing the infuse loader for paper

--- paper/src/main/java/org/turbojax/infusev1/paper/Main.java
+++ paper/src/main/java/org/turbojax/infusev1/paper/Main.java
@@ -2,12 +2,8 @@ package org.turbojax.infusev1.paper;
 
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.turbojax.infusev1.Infuse;
-import org.turbojax.infusev1.items.Enhancer;
-import org.turbojax.infusev1.items.InfuseEffect;
-import org.turbojax.infusev1.items.Reviver;
 import org.turbojax.infusev1.paper.commands.PaperInfuseCommand;
 import org.turbojax.infusev1.paper.listeners.*;
 
@@ -19,18 +15,11 @@ public class Main extends JavaPlugin {
 
         // Registering the command
         LifecycleEvents.COMMANDS.newHandler(e -> {
-             e.registrar().register(PaperInfuseCommand.buildPaper("infuse"));
+             e.registrar().register(PaperInfuseCommand.build("infuse"));
         });
 
-        // Registering recipes
-        Bukkit.addRecipe(new Enhancer().getRecipe().toBukkitRecipe(new NamespacedKey("infusev1", "enhancer")));
-        Bukkit.addRecipe(new InfuseEffect().getRecipe().toBukkitRecipe(new NamespacedKey("infusev1", "infuse_effect")));
-        Bukkit.addRecipe(new Reviver().getRecipe().toBukkitRecipe(new NamespacedKey("infusev1", "reviver")));
-
         // Registering listeners
-        Bukkit.getPluginManager().registerEvents(new InventoryCloseListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerInteractListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerItemConsumeListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerPostRespawnListener(), this);
