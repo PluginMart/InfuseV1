3e3e097d3ab2b1320521295862f069c4982dc367
TurboJax <turbojax07@gmail.com>
2026-09-06 17:12:26 -0400

    Fixing how recipes are reloaded on paper

--- paper/src/main/java/org/turbojax/infusev1/paper/InfusePaper.java
+++ paper/src/main/java/org/turbojax/infusev1/paper/InfusePaper.java
@@ -2,11 +2,15 @@ package org.turbojax.infusev1.paper;
 
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.craftbukkit.util.CraftNamespacedKey;
+import org.bukkit.entity.Player;
 import org.turbojax.infusev1.Infuse;
 import org.turbojax.infusev1.items.CustomItem;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 
 public class InfusePaper extends Infuse {
     @Override
@@ -21,11 +25,23 @@ public class InfusePaper extends Infuse {
 
     @Override
     public void reloadRecipes() {
-        CustomItem.getRegisteredItems().forEach((_, item) -> {
-            CraftingRecipe recipe = item.getRecipe();
-            if (recipe == null) return;
+        List<NamespacedKey> enabled = CustomItem.getRegisteredItems()
+            .values()
+            .stream()
+            .map(item -> {
+                CraftingRecipe recipe = item.getRecipe();
+                if (recipe == null) return null;
 
-            Bukkit.addRecipe(recipe.toBukkitRecipe(CraftNamespacedKey.fromMinecraft(item.id())));
-        });
+                var recipeId = CraftNamespacedKey.fromMinecraft(item.id());
+                Bukkit.removeRecipe(recipeId);
+                Bukkit.addRecipe(recipe.toBukkitRecipe(recipeId));
+                return recipeId;
+            })
+            .filter(Objects::nonNull)
+            .toList();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            enabled.forEach(player::discoverRecipe);
+        }
     }
 }
