235480e04004f09179bf94b80c08f1ed28849c45
TurboJax <turbojax07@gmail.com>
2026-09-06 15:54:28 -0400

    Fixing the death listener

--- fabric/src/main/java/org/turbojax/infusev1/fabric/mixins/ServerPlayerMixin.java
+++ fabric/src/main/java/org/turbojax/infusev1/fabric/mixins/ServerPlayerMixin.java
@@ -1,19 +1,18 @@
 package org.turbojax.infusev1.fabric.mixins;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.turbojax.infusev1.Infuse;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
-    @Inject(method="hurtServer", at=@At("TAIL"))
-    public void infusev1$onDeath(ServerLevel level, DamageSource source, float damage, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method="die", at=@At("HEAD"))
+    public void infusev1$onDeath(DamageSource source, CallbackInfo ci) {
         Infuse.getInstance().onDeath(self());
     }
 
