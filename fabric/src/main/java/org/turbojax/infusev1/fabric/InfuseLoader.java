66d18918de132b250af089b631da2c43f8da3170
TurboJax <turbojax07@gmail.com>
2026-09-06 17:04:46 -0400

    Fixing how recipes are reloaded on fabric

--- fabric/src/main/java/org/turbojax/infusev1/fabric/InfuseLoader.java
+++ fabric/src/main/java/org/turbojax/infusev1/fabric/InfuseLoader.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
@@ -13,6 +14,8 @@ import org.turbojax.infusev1.fabric.util.ReloadableResources;
 import org.turbojax.infusev1.items.CustomItem;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 
 public class InfuseLoader extends Infuse implements DedicatedServerModInitializer {
     public MinecraftServer server;
@@ -32,14 +35,31 @@ public class InfuseLoader extends Infuse implements DedicatedServerModInitialize
 
     @Override
     public void reloadRecipes() {
-        CustomItem.getRegisteredItems().forEach((key, item) -> {
-            CraftingRecipe recipe = item.getRecipe();
-            if (recipe == null) return;
+        var enabled = CustomItem.getRegisteredItems()
+                .values()
+                .stream()
+                .map(item -> {
+                    ResourceKey<Recipe<?>> recipeKey = ResourceKey.create(Registries.RECIPE, item.id());
 
-            ResourceKey<Recipe<?>> recipeKey = ResourceKey.create(Registries.RECIPE, item.id());
-            ((MutableRecipeManager) server.getRecipeManager()).infusev1$addRecipe(new RecipeHolder<>(recipeKey, recipe));
-        });
+                    // Removing the recipe so it can be re-added
+                    ((MutableRecipeManager) server.getRecipeManager()).infusev1$removeRecipe(recipeKey);
+
+                    CraftingRecipe recipe = item.getRecipe();
+                    if (recipe == null) return null;
+
+                    ((MutableRecipeManager) server.getRecipeManager()).infusev1$addRecipe(new RecipeHolder<>(recipeKey, recipe));
+                    return recipeKey;
+                })
+                .filter(Objects::nonNull)
+                .toList();
 
         ((ReloadableResources) server.getPlayerList()).infusev1$reloadRecipes();
+
+        // Reloading the recipes for the player.
+        for (Player player : server.getPlayerList().getPlayers()) {
+            List<RecipeHolder<?>> recipes = enabled.stream().flatMap((id) -> this.server.getRecipeManager().byKey(id).stream()).toList();
+            player.resetRecipes(recipes);
+            player.awardRecipes(recipes);
+        }
     }
 }
